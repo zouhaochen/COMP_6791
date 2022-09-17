e0d93651e402dc3d02a0127a72fba7a1b946900d
NormalZHC <zouhaochenluke@gmail.com>
2022-09-17 13:16:17 -0400

    tokenize

---
+++ Project1/Pipeline1.py
@@ -0,0 +1,32 @@
+from nltk import word_tokenize
+import os
+
+pipeline_input_file = "pipeline1.txt"
+pipeline_output_file = 'pipeline2.txt'
+output_dir = "Output/"
+
+
+if __name__ == '__main__':
+    counts = 0
+    actual_output_dir = output_dir + str(counts) + '/'
+    while os.path.exists(actual_output_dir):
+        for line in open(actual_output_dir + pipeline_input_file):
+            text = line.replace('\n', '')
+            tokens = word_tokenize(text)
+        special_symbols = '''!()-[]{};:'",<>./``''?@#$%^&*_~'''
+        token_list = []
+        # clean all special symbols in the tokens
+        for token in tokens:
+            have_digital = False
+            for character in token:
+                if character in '''0123456789''':
+                    have_digital = True
+                    break
+            if token not in special_symbols and token != '--' and not have_digital:
+                token_list.append(token)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        with open(actual_output_dir + pipeline_output_file, "w") as f:
+            f.write(str(token_list))
+        actual_output_dir = output_dir + str(counts) + '/'
+        counts += 1
