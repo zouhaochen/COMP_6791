e05cbb7ee03123ca7cd5dab56701adb6e2490099
NormalZHC <zouhaochenluke@gmail.com>
2022-09-17 14:05:39 -0400

    given a list of stop words, remove those stop words from text. Note that your code has to accept the stop word list as a parameter, do not hardcode a particular list

---
+++ Project1/Pipeline5.py
@@ -0,0 +1,23 @@
+import os
+
+pipeline_input_file = "pipeline4.txt"
+pipeline_output_file = 'pipeline5.txt'
+output_dir = "Output/"
+
+
+if __name__ == '__main__':
+    counts = 0
+    actual_output_dir = output_dir + str(counts) + '/'
+    print("please input the stop word split by ',' like to,a,the")
+    stop_word_list = input().split(',')
+    while os.path.exists(actual_output_dir):
+        for line in open(actual_output_dir + pipeline_input_file):
+            token_list = eval(line.replace('\n', ''))
+        token_without_stop_word_list = []
+        for token in token_list:
+            if token not in stop_word_list:
+                token_without_stop_word_list.append(token)
+        with open(actual_output_dir + pipeline_output_file, "w") as f:
+            f.write(str(token_without_stop_word_list))
+        actual_output_dir = output_dir + str(counts) + '/'
+        counts += 1
