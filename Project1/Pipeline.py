c0a1527a5c61a674fee0d13d667fb65637053c6e
NormalZHC <zouhaochenluke@gmail.com>
2022-09-18 13:09:16 -0400

    step 2 config

--- Project1/Pipeline.py
+++ Project1/Pipeline.py
@@ -4,22 +4,22 @@ import re
 
 # use NLTK for this project.
 import nltk
+from nltk import word_tokenize
 
 # download the Reuter's-21578 corpus onto computer.
 # use that version of the corpus, not the one available in NLTK.
 corpus_path = '../reuters21578'
-
-documents = []
 result_output_directory = 'Result/'
 step_one_output_file = 'step1.txt'
+step_two_output_file = 'step2.txt'
+
+documents = []
 
 
 # pipeline step one: read the Reuter's collection and extract the raw text of each article from the corpus
 def read_and_extract():
     for file_name in sorted(os.listdir(corpus_path)):
         if file_name.endswith('.sgm'):
-
-            # use latin-1 to decode some characters excluded in utf-8 or gbk
             with open(os.path.join(corpus_path, file_name), 'r', encoding='latin-1') as f:
                 reuters_file_content = f.read()
                 yield reuters_file_content
@@ -33,10 +33,9 @@ def step_one_execute():
         document_number = int(sys.argv[1])
 
     files = read_and_extract()
-    # iterate all .sgm files
     count = 0
+
     for file in files:
-        # get single document
         for document in re.findall("<REUTERS TOPICS.*?</REUTERS>", file.replace('\n', ' ')):
             title = re.search("<TITLE>.*?</TITLE>", document).group()[7: -8]
             body = re.search("<BODY>.*?</BODY>", document).group()[6: -7]
@@ -49,8 +48,46 @@ def step_one_execute():
                     f.write(content)
                 count += 1
             if count == document_number:
-                sys.exit()
+                return
+
+
+# execution of pipeline step two
+def step_two_execute():
+    count = 0
+    document_output_directory = result_output_directory + str(count + 1) + '/'
+
+    while os.path.exists(document_output_directory):
+        for line in open(document_output_directory + step_one_output_file):
+            content = line.replace('\n', '')
+            tokens = word_tokenize(content)
+
+        specific_symbol = '''!()-[]{};:'",<>./``''?@#$%^&*_~'''
+        token_list = []
+
+        for token in tokens:
+            digital_number = False
+            for character in token:
+                if character in '''0123456789''':
+                    digital_number = True
+                    break
+            if token not in specific_symbol and token != '--' and not digital_number:
+                token_list.append(token)
+
+        if not os.path.exists(result_output_directory):
+            os.makedirs(result_output_directory)
+
+        with open(document_output_directory + step_two_output_file, "w") as f:
+            f.write(str(token_list))
+        document_output_directory = result_output_directory + str(count + 1) + '/'
+        count += 1
 
 
 if __name__ == '__main__':
+
+    print("pipeline step one start.")
     step_one_execute()
+    print("pipeline step one finish.")
+
+    print("pipeline step two start.")
+    step_two_execute()
+    print("pipeline step two finish.")
