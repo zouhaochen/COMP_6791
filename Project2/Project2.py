995331c5c38e921e45496bdade1543af7c04f9e2
NormalZHC <zouhaochenluke@gmail.com>
2022-10-01 16:44:16 -0400

    project finish

--- Project2/Project2.py
+++ Project2/Project2.py
@@ -4,6 +4,11 @@ import nltk
 
 from nltk import word_tokenize
 from nltk import re
+from nltk import PorterStemmer
+from nltk.corpus import stopwords
+
+from tabulate import tabulate
+
 
 # download the Reuter's-21578 corpus onto computer.
 # use that version of the corpus, not the one available in NLTK.
@@ -20,6 +25,8 @@ punctuations = '''=\'+!()[]{};:",<>./`?@#$%^&*_~'''
 
 # dictionary key value pairs of terms-document ID in corpus
 dictionary = {}
+
+update_number = 0
 
 
 # read content from document files end with .sgm in the corpus
@@ -56,6 +63,7 @@ def store_in_disk():
         f.write(str(dictionary))
 
 
+# sub project one module
 def naive_indexer(F):
     files = read_from_file()
 
@@ -118,9 +126,12 @@ def naive_indexer(F):
                     F.append([token, document_id])
 
 
+# sub project two module
 def single_term_query_processing():
     with open(result_output_directory + sub_project_one_output_file, "r") as f:
         term_dictionary = eval(f.read())
+
+        # loop for processing single term query
         while True:
             print("please enter the single term query: ")
             term = input()
@@ -128,14 +139,201 @@ def single_term_query_processing():
             if posting_list is None:
                 print("no this term")
                 continue
+
             print("posting list length: " + str(len(posting_list)))
             print(posting_list)
+
             print("do you want to continue? y/n")
             choice = input()
             if choice == "y":
                 continue
             if choice == "n":
                 break
+
+
+# table of statistic about term without numbers
+def no_numbers(information, term_dictionary):
+    number = 0
+
+    for key in term_dictionary.keys():
+        number += len(term_dictionary.get(key))
+    information.get('number (non positional postings)').append(number)
+
+    keys_to_be_removed = []
+
+    for key in term_dictionary.keys():
+        if re.search("[0-9]", key) is not None:
+            keys_to_be_removed.append(key)
+
+    for key in keys_to_be_removed:
+        term_dictionary.pop(key)
+
+    global update_number
+
+    term_list = information.get('number (distinct terms)')
+    term_list.append(len(term_dictionary))
+    information.get('Δ %').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[1]) / term_list[0]))
+    information.get('T %').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[1]) / term_list[0]))
+
+
+# table of statistic about non positional postings
+def non_positional_postings(information, term_dictionary):
+    number = 0
+
+    for key in term_dictionary.keys():
+        number += len(term_dictionary.get(key))
+
+    term_list = information.get('number (non positional postings)')
+    term_list.append(number)
+    information.get('Δ % ').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[1]) / term_list[0]))
+    information.get('T % ').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[1]) / term_list[0]))
+
+
+# table of statistic about non positional postings with index
+def non_positional_postings_with_index(information, term_dictionary, index):
+    number = 0
+
+    for key in term_dictionary.keys():
+        number += len(term_dictionary.get(key))
+
+    term_list = information.get('number (non positional postings)')
+    term_list.append(number)
+    information.get('Δ % ').append('{:0.2f}'.format(-100 * (term_list[index] - term_list[index + 1]) / term_list[index]))
+    information.get('T % ').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[index + 1]) / term_list[0]))
+
+
+# merge list in order
+def merge_list(upper_list, lower_list):
+    temp_list = []
+    i = 0
+    j = 0
+
+    while i < len(upper_list) and j < len(lower_list):
+        if int(upper_list[i]) < int(lower_list[j]):
+            temp_list.append(upper_list[i])
+            i += 1
+        elif int(upper_list[i]) > int(lower_list[j]):
+            temp_list.append(lower_list[j])
+            j += 1
+        else:
+            temp_list.append(upper_list[i])
+            i += 1
+            j += 1
+
+    while j < len(lower_list):
+        temp_list.append(lower_list[j])
+        j += 1
+
+    while i < len(upper_list):
+        temp_list.append(upper_list[i])
+        i += 1
+
+    return temp_list
+
+
+# table of statistic about case folding
+def case_folding(information, term_dictionary):
+    new_term_dictionary = {}
+
+    for key in term_dictionary:
+        if new_term_dictionary.get(key.lower()) is None:
+            new_term_dictionary[key.lower()] = term_dictionary.get(key)
+        else:
+            new_term_dictionary[key.lower()] = merge_list(term_dictionary.get(key), new_term_dictionary[key.lower()])
+
+    term_list = information.get('number (distinct terms)')
+    term_list.append(len(new_term_dictionary))
+    information.get('Δ %').append('{:0.2f}'.format(-100 * (term_list[1] - term_list[2]) / term_list[1]))
+    information.get('T %').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[2]) / term_list[0]))
+    return new_term_dictionary
+
+
+# table of statistic about remove stop words from terms
+def remove_stop_word(information, term_dictionary, number, index):
+    stopwords_list = stopwords.words('english')[0: number - 1]
+    new_term_dictionary = {}
+
+    for key in term_dictionary.keys():
+        if key not in stopwords_list:
+            new_term_dictionary[key] = term_dictionary.get(key)
+
+    term_list = information.get('number (distinct terms)')
+    term_list.append(len(new_term_dictionary))
+    information.get('Δ %').append('{:0.2f}'.format(-100 * (term_list[2] - term_list[index]) / term_list[2]))
+    information.get('T %').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[index]) / term_list[0]))
+    return new_term_dictionary
+
+
+# table of statistic about stemming
+def stemming_list(information, term_dictionary, index):
+    new_term_dictionary = {}
+
+    for key in term_dictionary.keys():
+        term_after_stemming = PorterStemmer().stem(key)
+        if new_term_dictionary.get(term_after_stemming) is None:
+            new_term_dictionary[term_after_stemming] = term_dictionary.get(key)
+        else:
+            new_term_dictionary[term_after_stemming] = merge_list(new_term_dictionary[term_after_stemming], term_dictionary.get(key))
+
+    term_list = information.get('number (distinct terms)')
+    term_list.append(len(new_term_dictionary))
+    information.get('Δ %').append('{:0.2f}'.format(-100 * (term_list[index - 1] - term_list[index]) / term_list[index - 1]))
+    information.get('T %').append('{:0.2f}'.format(-100 * (term_list[0] - term_list[index]) / term_list[0]))
+    return new_term_dictionary
+
+
+# sub project three module
+def lossy_dictionary_compression_implementation():
+    with open(result_output_directory + sub_project_one_output_file, "r") as f:
+        term_dictionary = eval(f.read())
+
+    information = {' ': ['unfiltered', 'no numbers', 'case folding', '30 stop words', '150 stop words', 'stemming'],
+            'number (distinct terms)': [len(term_dictionary)],
+            'Δ %': [' '],
+            'T %': [' '],
+            'number (non positional postings)': [],
+            'Δ % ': [' '],
+            'T % ': [' ']
+            }
+
+    # remove numbers from terms
+    no_numbers(information, term_dictionary)
+    non_positional_postings(information, term_dictionary)
+
+    # case folding
+    term_dictionary = case_folding(information, term_dictionary)
+    non_positional_postings_with_index(information, term_dictionary, 1)
+
+    # remove 30 stop words
+    stopwords30_dic = remove_stop_word(information, term_dictionary, 30, 3)
+    non_positional_postings_with_index(information, stopwords30_dic, 2)
+
+    # remove 150 stop words
+    term_dictionary = remove_stop_word(information, term_dictionary, 150, 4)
+    non_positional_postings_with_index(information, term_dictionary, 3)
+
+    # implement stemming
+    term_dictionary = stemming_list(information, term_dictionary, 5)
+    non_positional_postings_with_index(information, term_dictionary, 4)
+
+    print(tabulate(information, headers='keys'))
+
+    while True:
+        print("please enter the single term query: ")
+        term = input()
+        posting_list = term_dictionary.get(PorterStemmer().stem(term))
+        if posting_list is None:
+            print("no such term.")
+            continue
+        print("term after stemmed: " + PorterStemmer().stem(term) + ", posting list length: " + str(len(posting_list)))
+        print(posting_list)
+
+        print("do you want to continue? y/n")
+        choice = input()
+        if choice == "y":
+            continue
+        if choice == "n":
+            break
 
 
 if __name__ == '__main__':
@@ -174,6 +372,12 @@ if __name__ == '__main__':
     single_term_query_processing()
     print("sub project 2 finish")
     print()
+
+    print("sub project 3 begin")
+    lossy_dictionary_compression_implementation()
+    print("sub project 3 finish")
+    print()
+
 
 
 
